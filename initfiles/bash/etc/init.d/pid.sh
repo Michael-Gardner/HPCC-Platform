487f6f232441408d110c31e9555810e0c00d8d3d
Michael Gardner <Michael.Gardner@lexisnexis.com>
2015-02-09 11:53:33 -0500

    HPCC-12923 Added sane logging/debugging to hpcc-init and component init files
    Signed-off-by: Michael Gardner <Michael.Gardner@lexisnexis.com>

--- initfiles/bash/etc/init.d/pid.sh
+++ initfiles/bash/etc/init.d/pid.sh
@@ -26,6 +26,7 @@ checkPidDir () {
         echo "Creating a Pid directory"
         /bin/mkdir -P ${PIDFILEPATH} 
         if [ !-e ${PIDFILEPATH} ]; then
+            log  "Can not create a Pid directory $PIDFILEPATH"
             echo "Can not create a Pid directory $PIDFILEPATH"
         else
             log_success_msg
@@ -42,22 +43,14 @@ createPid () {
     #echo -n "Creating Pid file $PIDFILEPATH"
     checkPid ${PIDFILEPATH}
     if [ $__flagPid -eq 1 ]; then
-        if [ ${DEBUG} != "NO_DEBUG" ]; then
-            log_failure_msg "Pid file already exists"
-        fi
         __pidCreated=0
     else
         echo $PIDNO > ${PIDFILEPATH} 
         checkPid ${PIDFILEPATH}
         if [ $__flagPid -eq 1 ]; then
-            if [ ${DEBUG} != "NO_DEBUG" ]; then
-                log_success_msg 
-            fi
             __pidCreated=1
         else
-            if [ ${DEBUG} != "NO_DEBUG" ]; then
-                log_failure_msg "Failed to create Pid"
-            fi
+            log "Failed to create Pid"
             __pidCreated=0
         fi
     fi
@@ -96,18 +89,13 @@ removePid () {
     #echo -n "Removing Pid file $PIDFILEPATH"
     checkPid ${PIDFILEPATH}
     if [ $__flagPid -eq 0 ]; then
-        if [ ${DEBUG} != "NO_DEBUG" ]; then
-            log_failure_msg "Pidfile doesn't exist"
-        fi
         __pidRemoved=0
     else
         /bin/rm -rf ${PIDFILEPATH}
         if [ ! -e ${PIDFILEPATH} ]; then
             __pidRemoved=1
         else
-            if [ ${DEBUG} != "NO_DEBUG" ]; then
-                log_failure_msg "Failed to remove pid"
-            fi
+            log "Failed to remove pid"
             __pidRemoved=0
         fi
     fi
@@ -160,19 +148,13 @@ check_status() {
 
     # check if running and healthy
     if [ $pidfilepathExists -eq 1 ] && [ $comppidfilepathExists -eq 1 ] && [ $componentLocked -eq 1 ] && [ $initRunning -eq 1 ] && [ $compRunning -eq 1 ]; then
-      if [ ${DEBUG} != "NO_DEBUG" ]; then
-        echo "everything is up except sentinel"
-      fi
+        log "$compName ---> Waiting on Sentinel"
       if [ ${SENTINELFILECHK} -eq 1 ]; then
         if [ ${sentinelFlag} -eq 0 ]; then
-          if [ ${DEBUG} != "NO_DEBUG" ]; then
-            echo "Sentinel is now up"
-          fi
+          log "$compName ---> Sentinel Up"
           return 0
         else
-          if [ ${DEBUG} != "NO_DEBUG" ]; then
-            echo "Sentinel not yet located, process currently unhealthy"
-          fi
+          log "$compName ---> Currently Unhealthy"
           return 2
         fi
       else
@@ -182,27 +164,21 @@ check_status() {
     elif [ $pidfilepathExists -eq 0 ] && [ $comppidfilepathExists -eq 0 ] && [ $componentLocked -eq 0 ] && [ $initRunning -eq 0 ] && [ $compRunning -eq 0 ]; then
       if [ ${SENTINELFILECHK} -eq 1 ]; then
         if [ ${sentinelFlag} -eq 0 ]; then
-          if [ ${DEBUG} != "NO_DEBUG" ]; then
-            echo "Sentinel is up but orphaned"
-          fi
+          log "$compName ---> Orphaned State"
           return 3
         else
-          if [ ${DEBUG} != "NO_DEBUG" ]; then
-            echo "Sentinel is now down"
-          fi
+          log "$compName ---> Sentinel Down"
           return 1
         fi
       else
         return 1
       fi
     else
-      if [ "${DEBUG}" != "NO_DEBUG" ]; then
-        [ $pidfilepathExists -eq 0 ]     && log_failure_msg "pid file path does not exist: $1"
-        [ $comppidfilepathExists -eq 0 ] && log_failure_msg "comp pid file path does not exist: $3"
-        [ $componentLocked -eq 0 ]       && log_failure_msg "component is not locked: $2"
-        [ $initRunning -eq 0 ]           && log_failure_msg "process for ${compName}_init.pid is not running"
-        [ $compRunning -eq 0 ]           && log_failure_msg "process for ${compName}.pid is not running"
-      fi
+      [ $pidfilepathExists -eq 0 ]     && log "$compName ---> pid file path does not exist: $1"
+      [ $comppidfilepathExists -eq 0 ] && log "$compName ---> comp pid file path does not exist: $3"
+      [ $componentLocked -eq 0 ]       && log "$compName ---> component is not locked: $2"
+      [ $initRunning -eq 0 ]           && log "$compName ---> process for ${compName}_init.pid is not running"
+      [ $compRunning -eq 0 ]           && log "$compName ---> process for ${compName}.pid is not running"
       return 4
     fi
 }
