0efdb83ed6d667c9617bbbe1eeb366f3ec7a07f0
Michael Gardner <Michael.Gardner@lexisnexis.com>
2015-06-05 09:43:07 -0400

    Altered check_status and moved kill_process
    Signed-off-by: Michael Gardner <Michael.Gardner@lexisnexis.com>

--- initfiles/bash/etc/init.d/pid.sh
+++ initfiles/bash/etc/init.d/pid.sh
@@ -54,7 +54,7 @@ removePid()
         log "Pid file doesn't exist"
         return 0
     else
-        checkPidExists $pidFile
+        checkPidRunning $pidFile
         if [[ $? -eq 0 ]]; then
             log "Process ($pidValue) is still running.  Failed to remove Pid File" 
             return 1
@@ -70,7 +70,7 @@ removePid()
     fi
 }
 
-checkPidExist()
+checkPidRunning()
 {
     getPid ${PIDFILEPATH}
     local pidVal=$?
@@ -84,8 +84,25 @@ checkPidExist()
 
 checkSentinelFile()
 {
-    filecheck=$(find ${runtime}/${compName} -name "*sentinel" 2> /dev/null)
-    [[ -n "${filecheck}" ]] && return 1
+    local filecheck=$(find ${runtime}/${compName} -name "*sentinel" 2> /dev/null)
+    [[ -n "${filecheck}" ]] && return 0
+    return 1
+}
+
+removeSentinelFile()
+{
+    checkSentinelFile
+    if [[ $? -eq 1 ]]; then
+        log "Sentinel file not found, cannot remove $sentinelFile"
+        return 1
+    fi
+    local sentinelFile=$(find ${runtime}/${compName} -name "*sentinel" 2> /dev/null)
+    rm -f $sentinelFile > /dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        log "Failed to remove sentinel file $sentinelFile"
+        return 1
+    fi
+    log "removed sentinel file $sentinelFile"
     return 0
 }
 
@@ -99,56 +116,55 @@ checkSentinelFile()
 #    Return:
 #       0: Running Healthy
 #       1: Stopped Healthy
-#       2: Running w/ no sentinel file
-#       3: Stopped except sentinel orphaned
-#       4: not yet in up state, details in debug mode
-check_status() {
-    PIDFILEPATH=$1
-    LOCKFILEPATH=$2
-    COMPPIDFILEPATH=$3
-    SENTINELFILECHK=$4
+#       2: Process starting up
+#       3: Orphaned Process
+check_status()
+{
+    local PIDFILEPATH=$1
+    local LOCKFILEPATH=$2
+    local COMPPIDFILEPATH=$3
+    local SENTINELFILECHK=$4
 
     locked $LOCKFILEPATH
     local componentLocked=$flagLocked
-    checkPidExist $PIDFILEPATH
-    local initRunning=$__pidExists
-    checkPidExist $COMPPIDFILEPATH
-    local compRunning=$__pidExists
+    checkPidRunning $PIDFILEPATH
+    local initRunning=$?
+    checkPidRunning $COMPPIDFILEPATH
+    local compRunning=$?
     checkSentinelFile
     local sentinelFlag=$?
 
     # check if running and healthy
-    if [[ $componentLocked -eq 1 ]] && [[ $initRunning -eq 1 ]] && [[ $compRunning -eq 1 ]]; then
+    if [[ $initRunning -eq 0 ]] && [[ $compRunning -eq 0 ]]; then
         [[ ${DEBUG} != "NO_DEBUG" ]] && echo "everything is up except sentinel"
         log "$compName ---> Waiting on Sentinel"
         if [[ ${SENTINELFILECHK} -eq 1 ]]; then
             if [[ ${sentinelFlag} -eq 0 ]]; then
-                [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel not yet located, process currently unhealthy"
-                log "$compName ---> Currently Unhealthy"
+                [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel not yet located, process in startup"
+                log "$compName ---> Process in startup"
                 return 2 
             fi
-            [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now up"
-            log "$compName ---> Sentinel Up"
         fi
+        [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now up"
+        log "$compName ---> Process is up"
         return 0
     # check if shutdown and healthy
-    elif [[ $componentLocked -eq 0 ]] && [[ $initRunning -eq 0 ]] && [[ $compRunning -eq 0 ]]; then
+    elif [[ $initRunning -eq 1 ]] && [[ $compRunning -eq 1 ]]; then
         if [[ ${SENTINELFILECHK} -eq 1 ]]; then
-            if [[ ${sentinelFlag} -eq 1 ]]; then
-                [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is up but orphaned"
-                log "$compName ---> Orphaned State"
-                return 3
+            if [[ ${sentinelFlag} -eq 0 ]]; then
+                removeSentinelFile
             fi
-            [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now down"
-            log "$compName ---> Sentinel Down"
         fi
+        [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Process is down"
+        log "$compName ---> Process is down"
         return 1
-    else
-        if [[ "${DEBUG}" != "NO_DEBUG" ]]; then
-            [[ $componentLocked -eq 0 ]] && log "$compName ---> component is not locked: $LOCKFILEPATH"
-            [[ $initRunning -eq 0 ]]     && log "$compName ---> process for init_${compName}.pid is not running"
-            [[ $compRunning -eq 0 ]]     && log "$compName ---> process for ${compName}.pid is not running"
-        fi
-        return 4
+    # component is orphaned somehow
+    elif [[ $initRunning -eq 1 ]] && [[ $compRunning -eq 0 ]]; then
+        log "$compName ---> Process is orphaned"
+        return 3
+    # Init process is already spawned
+    elif [[ $initRunning -eq 0 ]] && [[ $compRunning -eq 1 ]]; then
+        log "$compName ---> Process in startup"
+        return 2
     fi
 }
