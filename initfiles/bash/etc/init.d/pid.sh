ac2ee8513ac34e584cd8ceb8ad9f1ff33340b5db
Cloud User <centos@cent-7-sel.novalocal>
2016-10-18 14:42:15 +0000

    hpcc-15618 moved some files around, changes to driver
    Signed-off-by: Cloud User <centos@cent-7-sel.novalocal>

--- initfiles/bash/etc/init.d/pid.sh
+++ initfiles/bash/etc/init.d/pid.sh
@@ -38,17 +38,17 @@ createPid () {
     PIDNO=$2
     checkPid ${PIDFILEPATH}
     if [[ $__flagPid -eq 1 ]]; then
-        [[ ${DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Pid file already exists"
+        [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Pid file already exists"
         log "Pid file already exists"
         __pidCreated=0
     else
         echo $PIDNO > ${PIDFILEPATH}
         checkPid ${PIDFILEPATH}
         if [[ $__flagPid -eq 1 ]]; then
-            [[ ${DEBUG} != "NO_DEBUG" ]] && log_success_msg
+            [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && log_success_msg
             __pidCreated=1
         else
-            [[ ${DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Failed to create Pid"
+            [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Failed to create Pid"
             log "Failed to create Pid"
             __pidCreated=0
         fi
@@ -81,7 +81,7 @@ removePid () {
     PIDFILEPATH=$1
     checkPid ${PIDFILEPATH}
     if [[ $__flagPid -eq 0 ]]; then
-        [[ ${DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Pidfile doesn't exist"
+        [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Pidfile doesn't exist"
         log "Pid file doesn't exist"
         __pidRemoved=0
     else
@@ -89,7 +89,7 @@ removePid () {
         if [[ ! -e ${PIDFILEPATH} ]]; then
             __pidRemoved=1
         else
-            [[ ${DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Failed to remove pid"
+            [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && log_failure_msg "Failed to remove pid"
             log "Failed to remove pid"
             __pidRemoved=0
         fi
@@ -137,15 +137,15 @@ check_status() {
 
     # check if running and healthy
     if [[ $componentLocked -eq 1 ]] && [[ $initRunning -eq 1 ]] && [[ $compRunning -eq 1 ]]; then
-        [[ ${DEBUG} != "NO_DEBUG" ]] && echo "everything is up except sentinel"
+        [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && echo "everything is up except sentinel"
         log "$compName ---> Waiting on Sentinel"
         if [[ ${SENTINELFILECHK} -eq 1 ]]; then
             if [[ ${sentinelFlag} -eq 0 ]]; then
-                [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel not yet located, process currently unhealthy"
+                [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && echo "Sentinel not yet located, process currently unhealthy"
                 log "$compName ---> Currently Unhealthy"
                 return 2 
             fi
-            [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now up"
+            [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now up"
             log "$compName ---> Sentinel Up"
         fi
         return 0
@@ -153,16 +153,16 @@ check_status() {
     elif [[ $componentLocked -eq 0 ]] && [[ $initRunning -eq 0 ]] && [[ $compRunning -eq 0 ]]; then
         if [[ ${SENTINELFILECHK} -eq 1 ]]; then
             if [[ ${sentinelFlag} -eq 1 ]]; then
-                [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is up but orphaned"
+                [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is up but orphaned"
                 log "$compName ---> Orphaned State"
                 return 3
             fi
-            [[ ${DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now down"
+            [[ ${DEBUG:-NO_DEBUG} != "NO_DEBUG" ]] && echo "Sentinel is now down"
             log "$compName ---> Sentinel Down"
         fi
         return 1
     else
-        if [[ "${DEBUG}" != "NO_DEBUG" ]]; then
+        if [[ "${DEBUG:-NO_DEBUG}" != "NO_DEBUG" ]]; then
             [[ $componentLocked -eq 0 ]] && log "$compName ---> component is not locked: $LOCKFILEPATH"
             [[ $initRunning -eq 0 ]]     && log "$compName ---> process for init_${compName}.pid is not running"
             [[ $compRunning -eq 0 ]]     && log "$compName ---> process for ${compName}.pid is not running"
