2ba3dd98e9093a9b3069be2b90127e5dfe846ca4
Michael Gardner <michael.gardner@lexisnexis.com>
2017-03-29 12:40:00 -0400

    include-rinside
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexis.com>

--- cmake_modules/FindR.cmake
+++ cmake_modules/FindR.cmake
@@ -21,37 +21,57 @@
 #  R_INCLUDE_DIR - the R include directory(s)
 #  R_LIBRARIES - The libraries needed to use R
 
-IF (NOT R_FOUND)
-  IF (WIN32)
-    SET (R_lib "libR")
-    SET (Rcpp_lib "libRcpp")
-    SET (RInside_lib "libRInside")
-  ELSE()
-    SET (R_lib "R")
-    SET (Rcpp_lib "Rcpp")
-    SET (RInside_lib "RInside")
-  ENDIF()
+if(NOT R_FOUND)
+    if(WIN32)
+        set(R_lib "libR")
+        set(Rcpp_lib "libRcpp")
+        set(RInside_lib "libRInside")
+    else()
+        set(R_lib "R")
+        set(Rcpp_lib "Rcpp")
+        set(RInside_lib "RInside")
+    endif()
 
-  FIND_PATH(R_INCLUDE_DIR R.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/include)
-  FIND_PATH(RCPP_INCLUDE_DIR Rcpp.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/Rcpp/include/ R/site-library/Rcpp/include/)
-  FIND_PATH(RINSIDE_INCLUDE_DIR RInside.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/RInside/include  R/site-library/RInside/include)
+    find_path(R_INCLUDE_DIR R.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/include)
+    find_path(RCPP_INCLUDE_DIR Rcpp.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/Rcpp/include/ R/site-library/Rcpp/include/)
+    find_path(RINSIDE_INCLUDE_DIR RInside.h PATHS /usr/lib /usr/lib64 /usr/share /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/RInside/include  R/site-library/RInside/include)
 
-  FIND_LIBRARY (R_LIBRARY NAMES ${R_lib}  PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/lib)
-  FIND_LIBRARY (RCPP_LIBRARY NAMES ${Rcpp_lib} PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/Rcpp/lib/ R/site-library/Rcpp/lib/)
-  FIND_LIBRARY (RINSIDE_LIBRARY NAMES ${RInside_lib} PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/RInside/lib/ R/site-library/RInside/lib/)
+    find_library(R_LIBRARY NAMES ${R_lib}  PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/lib)
+    find_library(RCPP_LIBRARY NAMES ${Rcpp_lib} PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/Rcpp/lib/ R/site-library/Rcpp/lib/)
+    find_library(RINSIDE_LIBRARY NAMES ${RInside_lib} PATHS /usr/lib /usr/share /usr/lib64 /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/library/RInside/lib/ R/site-library/RInside/lib/)
 
-  IF (RCPP_LIBRARY STREQUAL "RCPP_LIBRARY-NOTFOUND")
-    SET (RCPP_LIBRARY "")    # Newer versions of Rcpp are header-only, with no associated library.
-  ENDIF()
+    if(RCPP_LIBRARY STREQUAL "RCPP_LIBRARY-NOTFOUND")
+        set(RCPP_LIBRARY "")    # Newer versions of Rcpp are header-only, with no associated library.
+    endif()
 
-  SET (R_INCLUDE_DIRS ${R_INCLUDE_DIR} ${RINSIDE_INCLUDE_DIR} ${RCPP_INCLUDE_DIR})
-  SET (R_LIBRARIES ${R_LIBRARY} ${RINSIDE_LIBRARY} ${RCPP_LIBRARY})
+    #Rcpp/config.h
+    #define RCPP_VERSION Rcpp_Version(0,12,3)
+    file(STRINGS "${RCPP_INCLUDE_DIR}/Rcpp/config.h" version_string REGEX "#define RCPP_VERSION Rcpp_Version\\(")
+    #major
+    string(REGEX REPLACE "#define RCPP_VERSION Rcpp_Version\\(" "" major "${version_string}")
+    string(REGEX REPLACE ",[0-9]+,[0-9]+\\)" "" major "${major}")
+    #minor
+    string(REGEX REPLACE "#define RCPP_VERSION Rcpp_Version\\([0-9]+," "" minor "${version_string}")
+    string(REGEX REPLACE ",[0-9]+\\)" "" minor "${minor}")
+    #patch
+    string(REGEX REPLACE "#define RCPP_VERSION Rcpp_Version\\([0-9]+,[0-9]+," "" patch "${version_string}")
+    string(REGEX REPLACE "\\)" "" patch "${patch}")
+    set(RCPP_VERSION_STRING "${major}.${minor}.${patch}")
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(R DEFAULT_MSG
-    R_LIBRARIES
-    R_INCLUDE_DIRS
-  )
+    set(R_INCLUDE_DIRS ${R_INCLUDE_DIR} ${RINSIDE_INCLUDE_DIR} ${RCPP_INCLUDE_DIR})
+    set(R_LIBRARIES ${R_LIBRARY} ${RINSIDE_LIBRARY} ${RCPP_LIBRARY})
 
-  MARK_AS_ADVANCED(R_INCLUDE_DIRS R_LIBRARIES)
-ENDIF()
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(R
+        REQUIRED_VARS R_LIBRARY
+                      RINSIDE_LIBRARY
+                      R_INCLUDE_DIR
+                      RCPP_INCLUDE_DIR
+                      RINSIDE_INCLUDE_DIR
+                      R_LIBRARIES
+                      R_INCLUDE_DIRS
+        VERSION_VAR RCPP_VERSION_STRING
+        )
+
+    mark_as_advanced(R_INCLUDE_DIRS R_LIBRARIES RINSIDE_LIBRARY)
+endif()
