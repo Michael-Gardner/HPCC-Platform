07145b6ccecfe83f7ddb780cf24ff2d068ae8a8f
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2018-11-01 19:01:53 -0400

    HPCC-20791 Remove libmemcached download from the build process
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- cmake_modules/FindLIBMEMCACHED.cmake
+++ cmake_modules/FindLIBMEMCACHED.cmake
@@ -24,8 +24,6 @@
 #  MEMCACHED_USE_EXTERNAL_LIBRARY is ON, we will fetch, build, and include a copy of the neccessary
 #  Libraries.
 
-option(MEMCACHED_USE_EXTERNAL_LIBRARY "Pull and build source from external location if local is not found" ON)
-
 # Search for native library to build against
 if(WIN32)
     set(libmemcached_lib "libmemcached")
@@ -55,61 +53,6 @@ if(LIBMEMCACHED_INCLUDE_DIR)
             set(MSG "libmemcached version '${LIBMEMCACHED_VERSION_STRING}' incompatible with min version>=${LIBMEMCACHED_FIND_VERSION}")
         endif()
     endif()
-endif()
-
-if((LIBMEMCACHEDCORE_LIBRARY STREQUAL "LIBMEMCACHEDCORE_LIBRARY-NOTFOUND"
-    OR LIBMEMCACHEDUTIL_LIBRARY STREQUAL "LIBMEMCACHEDUTIL_LIBRARY-NOTFOUND"
-    OR LIBMEMCACHED_INCLUDE_DIR STREQUAL "LIBMEMCACHED_INCLUDE_DIR-NOTFOUND"
-    OR NOT LIBMEMCACHED_VERSION_OK)
-    AND MEMCACHED_USE_EXTERNAL_LIBRARY)
-    # Currently libmemcached versions are not sufficient on ubuntu 12.04 and 14.04 LTS
-    # until then, we build the required libraries from source
-    if(NOT TARGET generate-libmemcached)
-        set(LIBMEMCACHED_URL https://launchpad.net/libmemcached/1.0/${LIBMEMCACHED_FIND_VERSION}/+download/libmemcached-${LIBMEMCACHED_FIND_VERSION}.tar.gz)
-        include(ExternalProject)
-        ExternalProject_Add(
-            generate-libmemcached
-            URL ${LIBMEMCACHED_URL}
-            DOWNLOAD_NO_PROGRESS 1
-            TIMEOUT 15
-            DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/downloads
-            SOURCE_DIR ${CMAKE_BINARY_DIR}/downloads/libmemcached
-            CONFIGURE_COMMAND "${CMAKE_BINARY_DIR}/downloads/libmemcached/configure" --prefix=${INSTALL_DIR} LDFLAGS=-L${LIB_PATH}
-            BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} LDFLAGS=-Wl,-rpath-link,${LIB_PATH}
-            BINARY_DIR ${CMAKE_BINARY_DIR}/build-libmemcached
-            INSTALL_COMMAND "")
-        add_library(libmemcached SHARED IMPORTED GLOBAL)
-        add_library(libmemcachedutil SHARED IMPORTED GLOBAL)
-        set_property(TARGET libmemcached
-            PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcached.so.11.0.0)
-        set_property(TARGET libmemcachedutil
-            PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcachedutil.so.2.0.0)
-        set_property(TARGET libmemcached
-            PROPERTY IMPORTED_LINK_DEPENDENT_LIBRARIES libmemcachedutil)
-        add_dependencies(libmemcached generate-libmemcached)
-        add_dependencies(libmemcachedutil generate-libmemcached)
-
-        if(PLATFORM)
-            install(CODE "set(ENV{LD_LIBRARY_PATH} \"\$ENV{LD_LIBRARY_PATH}:${CMAKE_BINARY_DIR}:${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs\")")
-            install(PROGRAMS
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcached.so
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcached.so.11
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcached.so.11.0.0
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcachedutil.so
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcachedutil.so.2
-                ${CMAKE_BINARY_DIR}/build-libmemcached/libmemcached/.libs/libmemcachedutil.so.2.0.0
-                DESTINATION lib)
-        endif()
-    endif()
-
-    set(LIBMEMCACHEDCORE_LIBRARY $<TARGET_FILE:libmemcached>)
-    set(LIBMEMCACHEDUTIL_LIBRARY $<TARGET_FILE:libmemcachedutil>)
-    set(LIBMEMCACHED_LIBRARIES $<TARGET_FILE:libmemcached> $<TARGET_FILE:libmemcachedutil>)
-    set(LIBMEMCACHED_INCLUDE_DIR ${CMAKE_BINARY_DIR}/downloads/libmemcached)
-    # always assumed to be ok
-    set(LIBMEMCACHED_VERSION_OK 1)
-else()
-    set(MEMCACHED_USE_EXTERNAL_LIBRARY OFF)
 endif()
 
 include(FindPackageHandleStandardArgs)
