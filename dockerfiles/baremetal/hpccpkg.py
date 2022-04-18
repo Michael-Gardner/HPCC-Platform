965748669eb7a6b7e2f4397f94095ec8f4a2df9b
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2022-04-18 13:10:28 -0400

    update to pkggen.py driver
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- dockerfiles/baremetal/hpccpkg.py
+++ dockerfiles/baremetal/hpccpkg.py
@@ -9,17 +9,16 @@ logger = logging.getLogger(__name__)
 class hpccpkg:
 
     def build(self):
-        logger.info(f"building tag {self.tag}")
         command = f"docker build -t {self.tag} \
             --build-arg branch={self.branch} \
             --build-arg repository={self.repository} \
             --build-arg cmake_build_type={self.cmake_build_type} \
             --build-arg check_git_tag={str(self.check_git_tag)} \
             {self.path}"
+        logger.info(f"building tag {self.tag}")
         logger.debug(f"running command - {command}")
         try:
-            process = subprocess.run(command.split(), capture_output=True,
-                timeout=900, text=True)
+            process = subprocess.run(command.split(), timeout=1200)
             logger.debug(process.stdout)
             if process.check_returncode():
                 logger.warning(process.stderr)
@@ -31,8 +30,7 @@ class hpccpkg:
         logger.info(f"creating temporary container tmp-{base_tag}")
         create_container = f"docker run --name tmp-{base_tag} {self.tag} /bin/true"
         try:
-            process = subprocess.run(create_container.split(), capture_output=True,
-                timeout=15, text=True)
+            process = subprocess.run(create_container.split(), timeout=15)
             logger.debug(process.stdout)
             if process.check_returncode():
                 logger.warning(process.stderr)
@@ -41,8 +39,7 @@ class hpccpkg:
         logger.info(f"copying artifacts for {self.tag} to local storage")
         copy_artifact = f"docker cp tmp-{base_tag}:/home/hpccbuild/package ."
         try:
-            process = subprocess.run(copy_artifact.split(), capture_output=True,
-            timeout=300, text=True)
+            process = subprocess.run(copy_artifact.split(), timeout=300)
             logger.debug(process.stdout)
             if process.check_returncode():
                 logger.warning(process.stderr)
@@ -54,15 +51,14 @@ class hpccpkg:
         logger.info(f"removing temporary container tmp-{base_tag}")
         cleanup_container = f"docker rm tmp-{base_tag}"
         try:
-            process = subprocess.run(cleanup_container.split(),
-                capture_output=True, text=True, timeout=120)
+            process = subprocess.run(cleanup_container.split(), timeout=120)
             logger.debug(process.stdout)
             if process.check_returncode():
                 logger.warning(process.stderr)
         except Exception as e:
             logger.exception(e)
 
-    def __init__(self, path, tag, repository='hpcc-systems', branch='master',
+    def __init__(self, path, tag, log=True, repository='hpcc-systems', branch='master',
         check_git_tag=False, cmake_build_type='Release'):
         self.path = path
         self.tag = tag
