47ac87b69b704926b2498751fac1ab8617ccbbc0
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2022-05-16 18:44:42 -0400

    Add push to base-containers and ubuntu2204 target
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- dockerfiles/baremetal/base-containers.py
+++ dockerfiles/baremetal/base-containers.py
@@ -35,6 +35,11 @@ def main():
             example: ubuntu1804, centos7
             '''
         )
+    parser.add_argument("-p", "--push", default=False, action='store_true',
+        help='''
+            Push image to dockerhub
+            default: False
+            ''')
     args = parser.parse_args()
     if args.target == 'all':
         target_platforms = ['centos7', 'centos8', 'ubuntu1804', 'ubuntu2004']
@@ -50,6 +55,10 @@ def main():
             process = subprocess.run(command.split(),
             text=True, check=True, cwd=cwd)
             print(" success")
+            if args.push:
+                command = f"docker push memeoru/hpcc-build-base:{target}"
+                process = subprocess.run(command.split(), text=True,
+                    check=True, cwd=cwd)
         except subprocess.CalledProcessError as e:
             print(" failed")
             print(e.stderr)
