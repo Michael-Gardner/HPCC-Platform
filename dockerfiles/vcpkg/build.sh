427ac16dd6f2a454d0084e41dccd0f11996d7ac9
Michael Gardner <michael.gardner@lexisnexisrisk.com>
2023-03-13 09:33:56 -0400

    work in progress 28915
    Signed-off-by: Michael Gardner <michael.gardner@lexisnexisrisk.com>

--- dockerfiles/vcpkg/build.sh
+++ dockerfiles/vcpkg/build.sh
@@ -40,14 +40,14 @@ function doBuild() {
 # docker run -it --mount source="$(pwd)",target=/hpcc-dev/HPCC-Platform,type=bind,consistency=cached build-ubuntu-22.04:latest bash
 }
 
-CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DVCPKG_FILES_DIR=/hpcc-dev -DCPACK_THREADS=0 -DUSE_OPTIONAL=OFF -DINCLUDE_PLUGINS=ON -DSUPPRESS_V8EMBED=ON"
+CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=RelWithDebInfo -DVCPKG_FILES_DIR=/hpcc-dev -DCPACK_THREADS=0 -DUSE_OPTIONAL=OFF -DINCLUDE_PLUGINS=ON -DSUPPRESS_V8EMBED=ON -DUSE_SHLIBDEPS=ON"
 
-doBuild amazonlinux
+#doBuild amazonlinux
 doBuild ubuntu-22.04 
-doBuild ubuntu-20.04
-doBuild ubuntu-18.04
-doBuild centos-8
-doBuild centos-7
+#doBuild ubuntu-20.04
+#doBuild ubuntu-18.04
+#doBuild centos-8
+#doBuild centos-7
 
 # docker build --progress plain --pull --rm -f "$SCRIPT_DIR/core.dockerfile" \
 #     -t $DOCKER_USERNAME/core:$GITHUB_REF \
